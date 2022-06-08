edbfb5a4083c4ce16301800a6e389de98a01fb48
hal <hal@sample.com>
2022-06-08 23:15:10 +0900

    beta

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use clap::{Command,Arg, ArgMatches};
-use std::{fs};
+use dirs::home_dir;
 
-const BASE_DIR_PATH : &str = "~/.local/bcp";
+use std::{fs::{self, Metadata}, env, path::Path, io::{Write, Read}};
+
 fn main() {
     let matches = Command::new("back up copy")
         .version("0.0.1")
@@ -39,25 +40,56 @@ fn main() {
 // 保存
 fn backup(matches : &ArgMatches) {
     println!("backup"); 
-    let src = matches.value_of("src");
-    println!("{}",src.unwrap());
-    let metadata = match fs::metadata(BASE_DIR_PATH) {
-        Ok(metadeta) => {metadeta},
-        Err(e) if e.raw_os_error().unwrap() == 2 => {
-            println!("error = {}",e);
-            println!("create dir");
-            // 無理なら諦めてパニックしておく。
-            fs::create_dir_all(BASE_DIR_PATH).unwrap();
-            fs::metadata(BASE_DIR_PATH).unwrap()
-        },
+    let sa = matches.value_of("src").unwrap();
+    let src = match Path::new(sa).canonicalize() {
+        Ok(p) => p,
         Err(e) => {
-            println!("error = {}",e);
-            std::process::exit(121)
+            eprintln!("{} does not exsist.",sa);
+            // panic!("error: {}",e.kind()) // errorの種類を調べたいなら
+            std::process::exit(120)
         }
     };
-    assert!(metadata.is_dir());
+    let abs_base_path = base_absolute_path();
+    // 保存先がなければ作成
+    match fs::metadata(abs_base_path.as_str()) {
+        Err(_) => {
+            // 無理ならパニック
+            fs::create_dir_all(abs_base_path.as_str()).unwrap();
+        },
+        _ => {}
+    }
+    let (sp,fine_name) = src.to_str().unwrap().rsplit_once("/").unwrap();
+    let repo = abs_base_path + sp;
+    let p = match Path::new(repo.clone().as_str()).canonicalize() {
+        Err(_) => {
+            // 無理ならパニック
+            fs::create_dir_all(repo.as_str()).unwrap();
+            Path::new(repo.as_str()).canonicalize().unwrap()
+        },
+        Ok(p) => {p}
+    };
+    // srcにサフィックスをつけて保存
+    let saved_file = String::from(p.to_str().unwrap()) + "/" + fine_name + "_test_suffix.txt" ;
+    println!("saved_file=[{}]",saved_file);
+    let mut f = std::fs::File::create(saved_file).unwrap();
+    let src_file = std::fs::File::open(src).unwrap();
+    for buf in src_file.bytes() {
+        f.write(&[buf.unwrap()]);
+    } 
 }
 
 fn restore(matches : &ArgMatches) {
     println!("restore");
+}
+
+fn base_absolute_path() -> String {
+    let home = home_dir().unwrap();
+    let home = home.to_str().unwrap();
+    String::from(home) + "/.local/bcp"
+}
+
+#[cfg(test)]
+mod Test {
+    use std::env;
+
 }
