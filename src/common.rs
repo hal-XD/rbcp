2f86e8c40591e334e09cd1885118a3f95a03442a
hal <hal@sample.com>
2022-06-11 03:06:56 +0900

    ファイル比較機能追加

--- src/common.rs
+++ src/common.rs
@@ -2,6 +2,9 @@ use std::path::PathBuf;
 use std::path::Path;
 
 use dirs::home_dir;
+use lazy_static::lazy_static;
+use regex::Regex;
+
 
 pub fn base_absolute_path() -> String {
     let home = home_dir().unwrap();
@@ -17,5 +20,34 @@ pub fn get_abs_path(relative_path:&str) -> PathBuf {
             // panic!("error: {}",e.kind()) // errorの種類を調べたいなら
             std::process::exit(120)
         }
+    }
+}
+
+pub fn check_filename_format(fname:&str) -> bool {
+    lazy_static! {
+        static ref RE : Regex = Regex::new(r"^.*_[0-9]{8}-[0-9]{6}_.*$").unwrap();
+    }
+    RE.is_match(fname)
+}
+
+#[cfg(test)]
+mod common_test {
+    use crate::common::check_filename_format;
+
+
+    #[test]
+    fn test_regex() {
+        let fname = "abcd_20221231-231140_nocomment";
+        assert!(check_filename_format(fname));
+        let fname = "abcd_20221231-231140_Why2022-12-1";
+        assert!(check_filename_format(fname));
+        let fname = "abcd_2022123112-231140_Why2022-12-1";
+        assert!(!check_filename_format(fname));
+        let fname = "20221231-240000-_2022123112-231140_Why2022-12-1";
+        assert!(!check_filename_format(fname));
+        let fname = "abcd_20221231-231140";
+        assert!(!check_filename_format(fname));
+        let fname = "foo.rs_20220609-230838_nocomment";
+        assert!(check_filename_format(fname));
     }
 }
--- src/restore.rs
+++ src/restore.rs
@@ -4,6 +4,7 @@ use std::fs::{read_dir,metadata, DirEntry};
 
 use crate::common::{self, base_absolute_path} ;
 
+
 pub fn restore(matches : &ArgMatches) {
     let target = matches.value_of("src").unwrap();
     let abs_src = common::get_abs_path(target);
@@ -38,18 +39,35 @@ pub fn restore(matches : &ArgMatches) {
     for entry in vec {
         let fname = String::from(entry.file_name().to_str().unwrap());
         //println!("candidate=[{}]",fname);
-        if compare_timestamp(tmp.as_str(), fname.as_str()) {
+        if compare_timestamp(tmp.as_str(), fname.as_str(),file_name) {
             tmp = fname;
         };
     }
     //println!("target   =[{}]",tmp);
-    replace_file(abs_src.as_path(), abs_target + path_to_src + tmp.as_str())
+    println!("{}",tmp);
+    replace_file(abs_src.to_str().unwrap(), abs_target + path_to_src + tmp.as_str())
 }
 
-fn replace_file<A,B>(src: A,dest: B) {
-
+fn replace_file(src: &str,dest: String)
+{
+    let mut y = dest.clone();
 }
 
-fn compare_timestamp<T>(first:T,second:T) -> bool {
-    true
+fn compare_timestamp(first:&str, second:&str, filename: &str) -> bool {
+    // _yyyymmdd-HHSSMM_ 16
+    if !common::check_filename_format(first) {
+        return true;
+    }
+    if !common::check_filename_format(second) {
+        return false;
+    }
+    let first = first.replace(filename, "");
+    let (s_date,_) = first.split_at(16);
+    let second = second.replace(filename, "");
+    let (d_date,_) = second.split_at(16); 
+    d_date.replace("_","")
+        .replace("-", "").parse::<u64>().unwrap()
+    > s_date.replace("_", "")
+        .replace("-", "").parse::<u64>().unwrap()
 }
+
